116fae6109fd407239ff68e9b7678282ca4b01d0
Rodrigo <contactorodrigocabrera@gmail.com>
2023-08-11 19:49:17 -0300

    Actualización

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-//import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Saludo from './components/saludo/Saludo';
 
+
 function App() {
+
+  let estilos = {
+    color: 'orange',
+    border: '1px black solid',
+    paddingTop: 10,
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Saludo></Saludo>
-      </header>
+    <div className="App" >
+      <h1 style={estilos}>React App</h1>
     </div>
   );
 }
